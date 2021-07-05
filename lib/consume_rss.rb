d6d449a84a6d2cf0b287313ec184466391310361
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-07-05 15:46:55 +0700

    added input many rss feed functionality

--- lib/consume_rss.rb
+++ lib/consume_rss.rb
@@ -3,16 +3,19 @@ require 'open-uri'
 
 class ConsumeRss
   def consume(url)
-    URI.open(url) do |rss|
-      feed = RSS::Parser.parse(rss)
-      puts "\n"
-      puts "Title: #{feed.channel.title}"
-      feed.items.each do |item|
-        puts '---------------------'
-        puts "Item: #{item.title}"
-        puts "Description: #{item.description}"
-        puts "Link: #{item.link}"
+    puts url
+    url.each do |rss_feed|
+      URI.open(rss_feed) do |rss|
+        feed = RSS::Parser.parse(rss)
         puts "\n"
+        puts "Title: #{feed.channel.title}"
+        feed.items.each do |item|
+          puts '---------------------'
+          puts "Item: #{item.title}"
+          puts "Description: #{item.description}"
+          puts "Link: #{item.link}"
+          puts "\n"
+        end
       end
     end
   end
--- lib/index.rb
+++ lib/index.rb
@@ -2,10 +2,17 @@ require_relative './consume_rss'
 require 'open-uri'
 
 def main
-  puts 'Masukkan link RSS anda'
-  inputed_rss = gets.chomp
+  inputed_rss_arr = []
+  loop do
+    puts 'Masukkan feed RSS anda'
+    inputed_rss = gets.chomp
+    inputed_rss_arr << inputed_rss
+    puts 'Apakah anda mau memasukkan feed RSS lagi?'
+    user_choice = gets.chomp
+    break if user_choice == 'Tidak'
+  end
   rss_consumer = ConsumeRss.new
-  rss_consumer.consume(inputed_rss)
+  rss_consumer.consume(inputed_rss_arr)
 end
 
 main
